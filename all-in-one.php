9bc64abfa50303c733ddb913238d325c0a9affbc
rajesh-ccs <rajesh.mp@ccstechnologies.in>
2017-06-02 17:23:45 +0530

    changes

--- all-in-one.php
+++ all-in-one.php
@@ -60,7 +60,7 @@ if ($android) {
     
             <? if ($ios): ?>
         
-            <?php header('Location:'.$ios_install.''); ?>
+            <?php header('Location:'.$ios_install.''); exit; ?>
          
             
             <? elseif ($android): ?>
